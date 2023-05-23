2ab263e6e641bd5138666edde67cfae44b90b7ad
DeonaKoci <127995793+DeonaKoci@users.noreply.github.com>
2023-05-23 21:25:01 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -6,9 +6,9 @@ float funzione(int v){
     float array[v];
     float b=array[v];
     float r;
-    cout << "inserire i valori dell'array uno dopo l'altro"<<endl;
+    //cout << "inserire i valori dell'array uno dopo l'altro"<<endl;
     for(i=0;i<v;i++){
-        cout<< "inserire il valore n ""+i+""dell'array "<<endl;
+       // cout<< "inserire il valore n ""+i+""dell'array "<<endl;
         cin >> array[i];
     }r=array[0];
     for(i=1;i<v;i++){
