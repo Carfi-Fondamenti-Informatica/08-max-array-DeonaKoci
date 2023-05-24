2f7a5bdc30355b08c7f85b06cbef24f8d9c79d82
DeonaKoci <127995793+DeonaKoci@users.noreply.github.com>
2023-05-24 19:54:42 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
     //cout<< "inserire dim"<< endl;
     cin>> dim;
     float array[dim];
-    for(int i=1; i<=dim; i++){
+    for(int i=0; i<dim; i++){
        cin >> array[i];
     }
     cout << funzione(dim,array) << endl;
