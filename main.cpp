5b4ece4bbec95069f8afeb999f31004541c70a0e
DeonaKoci <127995793+DeonaKoci@users.noreply.github.com>
2023-05-24 19:43:57 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,19 +2,13 @@
 #include "lib.h"
 using namespace std;
 
-
-int main() {
-
-    float max=0;
-    int dim=0;
+   int dim=0;
     //cout<< "inserire dim"<< endl;
     cin>> dim;
     float array[dim];
     for(int i=1; i<=dim; i++){
        cin >> array[i];
     }
-    max = funzione(dim,array);
-    cout << max;
-
+    cout << funzione(dim,array) << endl;
     return 0;
 }
