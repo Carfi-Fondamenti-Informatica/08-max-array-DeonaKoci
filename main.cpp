fa780d06412cc4ee7e0983ce58a75a5e89e5dd15
DeonaKoci <127995793+DeonaKoci@users.noreply.github.com>
2023-05-24 17:15:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 #include "lib.h"
 using namespace std;
-int main(){
-    int dim;
-    float array[dim];
-    
-    //cout << "inserire dimensione dell'array"<< endl;
-    cin >> dim;
 
-    //cout << "inserire i valori dell'array uno dopo l'altro"<<endl;
-    for(int i=0;i<dim;i++){
-        //cout<< "inserire il valore n ""+i+ ""dell'array "<<endl;
-        cin >> array[i];
+
+int main() {
+
+    float max=0;
+    int dim=0;
+    //cout<< "inserire dim"<< endl;
+    cin>> dim;
+    float array[dim];
+    for(int i=1; i<=dim; i++){
+       cin >> array[i];
     }
-   float max=funzione(dim);
-    //return y;
+    max = funzione(dim,array);
     cout << max;
 
-return 0;
+    return 0;
 }
