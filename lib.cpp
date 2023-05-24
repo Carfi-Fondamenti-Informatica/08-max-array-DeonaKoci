acb705521a370dd7a2af880cf75c5ff6c846f744
DeonaKoci <127995793+DeonaKoci@users.noreply.github.com>
2023-05-24 19:55:50 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,8 +1,8 @@
 #include "lib.h"
 float funzione(int dim, float array[]){
 
-    float max=array[1];
-    for(int i=1; i<=dim ; i++){
+    float max=array[0];
+    for(int i=0; i<dim ; i++){
 
         if( array[i] > max ){
             max=array[i];
